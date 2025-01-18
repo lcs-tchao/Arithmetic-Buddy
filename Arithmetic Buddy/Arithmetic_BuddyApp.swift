09982fdb45ba398f26abe3faadb93a2177828b66
Thomas Chao <Tchao@lcs.on.ca>
2025-01-18 10:40:19 -0500

    Initial Commit

---
+++ Arithmetic Buddy/Arithmetic_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_BuddyApp.swift
+//  Arithmetic Buddy
+//
+//  Created by Yuk Yeung Chao on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
